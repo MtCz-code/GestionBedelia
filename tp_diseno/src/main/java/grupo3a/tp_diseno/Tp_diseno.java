846d853142fc3eb5df139bafa162ecc5851c2d7c
Gabriel J Canavesio <gabrielcanavesio17@gmail.com>
2024-10-22 19:51:28 -0300

    creacion proyecto java

---
+++ tp_diseno/src/main/java/grupo3a/tp_diseno/Tp_diseno.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package grupo3a.tp_diseno;
+
+/**
+ *
+ * @author gabic
+ */
+public class Tp_diseno {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
