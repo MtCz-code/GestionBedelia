32008f770098baeaa828d51ebccb74b8d46b0032
Gabriel J Canavesio <gabrielcanavesio17@gmail.com>
2024-10-28 23:00:55 -0300

    algunas cositas

--- tp_diseno/src/main/java/grupo3a/tp_diseno/Gestores/GestorReserva.java
+++ tp_diseno/src/main/java/grupo3a/tp_diseno/Gestores/GestorReserva.java
@@ -1,6 +1,22 @@
 
 package grupo3a.tp_diseno.Gestores;
 
+import java.util.*;
+import grupo3a.tp_diseno.Modelos.Reserva;
+import grupo3a.tp_diseno.DTOs.DetalleReservaDTO;
+import grupo3a.tp_diseno.DTOs.ReservaDTO;
+
 public class GestorReserva {
+
+    public boolean validarDias(List<DetalleReservaDTO> diasReserva){
+        
+        return true;
+    }
+    
+    public boolean validarDatosReserva(ReservaDTO reserva){
+        
+        return true;
+    }
+    
     
 }
