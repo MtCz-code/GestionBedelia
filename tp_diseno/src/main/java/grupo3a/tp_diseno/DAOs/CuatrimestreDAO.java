7d85f0bf5369e622536a05ac5ddda2bc4a924d4a
ExequielSandria <sandriaexequiel@gmail.com>
2024-12-10 19:50:55 -0300

    extras a cuatrimesters

--- tp_diseno/src/main/java/grupo3a/tp_diseno/DAOs/CuatrimestreDAO.java
+++ tp_diseno/src/main/java/grupo3a/tp_diseno/DAOs/CuatrimestreDAO.java
@@ -6,6 +6,7 @@ package grupo3a.tp_diseno.DAOs;
 
 import grupo3a.tp_diseno.Exceptions.Exceptions.DAOException;
 import grupo3a.tp_diseno.Modelos.Cuatrimestre;
+import java.time.Year;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -16,4 +17,6 @@ import java.util.List;
 public interface CuatrimestreDAO {
     public Cuatrimestre buscarPorId(int id);
     public List<Cuatrimestre> getCuatrimestresActuales() throws DAOException;
+    public List<Year> recuperarAñosCuatrimestres() throws DAOException;
+    public List<Cuatrimestre> recuperarCuatrimestresPorAño(Year año) throws DAOException;
 }
